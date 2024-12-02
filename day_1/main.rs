a73c1d1a98c9d41ad2e954a1257629a62f6b1732
chirag-ghosh <cghosh828049@gmail.com>
2024-12-02 23:05:10 +0530

    feat: add day 1
    Signed-off-by: chirag-ghosh <cghosh828049@gmail.com>

---
+++ day_1/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use std::collections::HashMap;
+
+fn main() {
+    let input_file_contents = fs::read_to_string("input.txt").unwrap();
+    let mut first_vector = Vec::<i32>::new();
+    let mut second_vector = Vec::<i32>::new();
+    let mut first_map = HashMap::<i32, i32>::new();
+    let mut second_map = HashMap::<i32, i32>::new();
+
+    for line in input_file_contents.lines() {
+        let line_split = line.split_whitespace().collect::<Vec<&str>>();
+        let first_entry = line_split[0].parse::<i32>().unwrap();
+        let second_entry = line_split[1].parse::<i32>().unwrap();
+
+        first_vector.push(first_entry);
+        first_map.entry(first_entry).and_modify(|e| *e += 1).or_insert(1);
+
+        second_vector.push(second_entry);
+        second_map.entry(second_entry).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    first_vector.sort();
+    second_vector.sort();
+
+    let mut distance = 0;
+    for i in 0..first_vector.len() {
+        distance += (first_vector[i] - second_vector[i]).abs();
+    }
+
+    println!("Distance: {}", distance);
+
+    let mut similarity = 0;
+    for key in first_map.keys() {
+        if second_map.contains_key(key) {
+            similarity += key * second_map.get(key).unwrap();
+        } 
+    }
+
+    println!("Similarity: {}", similarity);
+}
