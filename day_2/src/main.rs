079b7f9c2e08a105d56fe78a42808249349bc6db
chirag-ghosh <cghosh828049@gmail.com>
2024-12-03 01:27:59 +0530

    feat: add day 2b
    Signed-off-by: chirag-ghosh <cghosh828049@gmail.com>

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -12,41 +12,56 @@ fn get_diff_vec(line_vec: Vec<i32>) -> Vec<i32> {
     return line_diff_vec;
 }
 
+fn is_safe(line_diff_vec: Vec<i32>) -> bool {
+    if line_diff_vec.len() == 0 {
+        return true;
+    }
+
+    let is_increasing = line_diff_vec[0] > 0;
+    let mut is_safe = true;
+    for &diff in line_diff_vec.iter() {
+        if diff == 0 {
+            is_safe = false;
+            break;
+        } else if is_increasing && diff < 0 {
+            is_safe = false;
+            break;
+        } else if !is_increasing && diff > 0 {
+            is_safe = false;
+            break;
+        } else if diff.abs() > 3 {
+            is_safe = false;
+            break;
+        }
+    }
+
+    return is_safe;
+}
+
 fn main() {
     let input_file_contents = fs::read_to_string("src/input.txt").unwrap();
 
     let mut safe_reports_count = 0;
+    let mut safe_reports_count_adj = 0;
     for line in input_file_contents.lines() {
         let line_vec: Vec<i32> = line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let line_diff_vec = get_diff_vec(line_vec);
+        let line_diff_vec = get_diff_vec(line_vec.to_vec());
 
-        if line_diff_vec.len() == 0 {
+        if is_safe(line_diff_vec) {
             safe_reports_count += 1;
-            continue;
-        }
-
-        let is_increasing = line_diff_vec[0] > 0;
-        let mut is_safe = true;
-        for &diff in line_diff_vec.iter() {
-            if diff == 0 {
-                is_safe = false;
-                break;
-            } else if is_increasing && diff < 0 {
-                is_safe = false;
-                break;
-            } else if !is_increasing && diff > 0 {
-                is_safe = false;
-                break;
-            } else if diff.abs() > 3 {
-                is_safe = false;
-                break;
+            safe_reports_count_adj += 1;
+        } else {
+            for index_to_remove in 0..line_vec.len() {
+                let mut line_vec_clone = line_vec.to_vec();
+                line_vec_clone.remove(index_to_remove);
+                if is_safe(get_diff_vec(line_vec_clone)) {
+                    safe_reports_count_adj += 1;
+                    break;
+                }
             }
-        }
-
-        if is_safe {
-            safe_reports_count += 1;
         }
     }
 
     println!("Safe reports count: {}", safe_reports_count);
+    println!("Safe reports count (adjusted): {}", safe_reports_count_adj);
 }
