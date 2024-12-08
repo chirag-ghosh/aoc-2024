ddebe02946d74bb5304d18c82344aa7f7f4604a5
chirag-ghosh <cghosh828049@gmail.com>
2024-12-08 12:12:41 +0530

    feat: add day 8b
    Signed-off-by: chirag-ghosh <cghosh828049@gmail.com>

--- src/day_8.rs
+++ src/day_8.rs
@@ -12,7 +12,7 @@ pub fn main(input_string: String) -> (String, String) {
     let mut antenna_map: HashMap<char, Vec<(i32,i32)>> = HashMap::new();
     for (line_index, line) in lines.into_iter().enumerate() {
         for (point_index, point) in line.chars().into_iter().enumerate() {
-            if point == '.' {
+            if point == '.' || point == '#' {
                 continue;
             }
             if antenna_map.contains_key(&point) {
@@ -24,19 +24,21 @@ pub fn main(input_string: String) -> (String, String) {
     }
 
     let mut antinodes: HashSet<(i32,i32)> = HashSet::new();
-    for (_, antenna_locations) in antenna_map.into_iter() {
+    for (_, antenna_locations) in antenna_map.clone().into_iter() {
         for i in 0..(antenna_locations.len()-1) {
             for j in (i+1)..antenna_locations.len() {
                 let new_point_1 = (
                     antenna_locations[i].0 + (antenna_locations[i].0 - antenna_locations[j].0),
-                    antenna_locations[i].1 + (antenna_locations[i].1 - antenna_locations[j].1));
+                    antenna_locations[i].1 + (antenna_locations[i].1 - antenna_locations[j].1)
+                );
                 if check_point_bounds(new_point_1, rows_len, columns_len) {
                     antinodes.insert(new_point_1);
                 }
 
                 let new_point_2 = (
                     antenna_locations[j].0 + (antenna_locations[j].0 - antenna_locations[i].0),
-                    antenna_locations[j].1 + (antenna_locations[j].1 - antenna_locations[i].1));
+                    antenna_locations[j].1 + (antenna_locations[j].1 - antenna_locations[i].1)
+                );
                 if check_point_bounds(new_point_2, rows_len, columns_len) {
                     antinodes.insert(new_point_2);
                 }
@@ -44,5 +46,38 @@ pub fn main(input_string: String) -> (String, String) {
         }
     }
 
-    (antinodes.len().to_string(), 0.to_string())
+    let mut extended_antinodes: HashSet<(i32, i32)> = HashSet::new();
+    for (_, antenna_locations) in antenna_map.clone().into_iter() {
+        for i in 0..(antenna_locations.len()-1) {
+            for j in (i+1)..antenna_locations.len() {
+                let mut new_point_1 = antenna_locations[i];
+                loop {
+                    if check_point_bounds(new_point_1, rows_len, columns_len) {
+                        extended_antinodes.insert(new_point_1);
+                    } else {
+                        break;
+                    }
+                    new_point_1 = (
+                        new_point_1.0 + (antenna_locations[i].0 - antenna_locations[j].0),
+                        new_point_1.1 + (antenna_locations[i].1 - antenna_locations[j].1)
+                    );
+                }
+
+                let mut new_point_2 = antenna_locations[j];
+                loop {
+                    if check_point_bounds(new_point_2, rows_len, columns_len) {
+                        extended_antinodes.insert(new_point_2);
+                    } else {
+                        break;
+                    }
+                    new_point_2 = (
+                        new_point_2.0 + (antenna_locations[j].0 - antenna_locations[i].0),
+                        new_point_2.1 + (antenna_locations[j].1 - antenna_locations[i].1)
+                    );
+                }
+            }
+        }
+    }
+
+    (antinodes.len().to_string(), extended_antinodes.len().to_string())
 }
