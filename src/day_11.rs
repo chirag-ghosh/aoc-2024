71c3b9c1afeeec81f9ee29811fd6e607b4b372f3
chirag-ghosh <cghosh828049@gmail.com>
2024-12-12 16:57:57 +0530

    feat: add day 11b
    Signed-off-by: chirag-ghosh <cghosh828049@gmail.com>

--- src/day_11.rs
+++ src/day_11.rs
@@ -1,22 +1,44 @@
-pub fn main(input_string: String) -> (String, String) {
-    let mut stone_line = input_string.trim().split_whitespace().map(String::from).collect::<Vec<String>>();
+use std::collections::HashMap;
 
-    for _ in 0..25 {
-        let mut stone_line_new: Vec<String> = Vec::new();
-        for stone in stone_line.to_owned().into_iter() {
-            if stone == "0" {
-                stone_line_new.push("1".to_owned());
-            } else if stone.len() % 2 == 0 {
-                let stone_split = stone.split_at(stone.len() / 2);
-                stone_line_new.push(stone_split.0.parse::<i64>().unwrap().to_string());
-                stone_line_new.push(stone_split.1.parse::<i64>().unwrap().to_string());
-            } else {
-                let stone_value = stone.parse::<i64>().unwrap();
-                stone_line_new.push((stone_value * 2024).to_string());
-            }
-        }
-        stone_line = stone_line_new;
+fn get_stone_line_len(lookup: &mut HashMap<(String, i32), i64>, input_stone: String, remaining_blinks: i32) -> i64 {
+    if remaining_blinks == 0 {
+        return 1;
     }
 
-    (stone_line.len().to_string(), 0.to_string())
+    if lookup.contains_key(&(input_stone.to_owned(), remaining_blinks)) {
+        return *lookup.get(&(input_stone, remaining_blinks)).unwrap();
+    }
+
+    let mut num_stones = 0;
+    if input_stone == "0" {
+        num_stones = get_stone_line_len(lookup, "1".to_owned(), remaining_blinks - 1);
+    } else if input_stone.len() % 2 == 0 {
+        let stone_split = input_stone.split_at(input_stone.len() / 2);
+        num_stones += get_stone_line_len(lookup, stone_split.0.parse::<i64>().unwrap().to_string(), remaining_blinks - 1);
+        num_stones += get_stone_line_len(lookup, stone_split.1.parse::<i64>().unwrap().to_string(), remaining_blinks - 1);
+    } else {
+        num_stones = get_stone_line_len(lookup, (input_stone.parse::<i64>().unwrap() * 2024).to_string(), remaining_blinks - 1)
+    }
+
+    lookup.insert((input_stone, remaining_blinks), num_stones);
+
+    return num_stones;
+}
+
+pub fn main(input_string: String) -> (String, String) {
+    let stone_line = input_string.trim().split_whitespace().map(String::from).collect::<Vec<String>>();
+
+    let mut lookup: HashMap<(String, i32), i64> = HashMap::new();
+
+    let mut stone_len_25 = 0;
+    for stone in stone_line.to_owned().into_iter() {
+        stone_len_25 += get_stone_line_len(&mut lookup, stone, 25);
+    }
+
+    let mut stone_len_75 = 0;
+    for stone in stone_line.to_owned().into_iter() {
+        stone_len_75 += get_stone_line_len(&mut lookup, stone, 75);
+    }
+
+    (stone_len_25.to_string(), stone_len_75.to_string())
 }
